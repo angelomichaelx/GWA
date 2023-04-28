4a7fb723f827ec6f37a2bd96e0584bf32e70846b
Michael Angelo P. Biag <michaelangelobiag23@GMAIL.COM>
2023-04-29 02:53:09 +0800

    print best_student + is the student with the highest GWA of + highest gwa and final edit

--- gwa.py
+++ gwa.py
@@ -2,21 +2,25 @@
 #BSCPE 1-4
 # Write a Python program that read a file containing the name of 20 students together with their GWA. The program will outputs the name of the student who got the highest GWA (including the GWA).
 
-#before we start we need to add the list of students with grade
-with open("studentlist.txt", "r") as file:
-    for line in file:   
-
 #Create two variables with empty and no initial values to which we will append the values after analyzing each line of the text.
-        highest_gwa = None
-        best_student = ""
+highest_gwa = None
+best_student = ""
+
+#before we start we need to add the list of students with grade
+with open("studentlist.txt", "r") as A:
+    for line in A:   
 
 #create two part, one is for student and the second one is for their grades
-name, gwa = line.strip().split(" - ")
-gwa = float(gwa)
+        name, gwa = line.strip().split(" - ")
+        gwa = float(gwa)
 
 #compute and pick the highest gwa in all 20 student
-if highest_gwa is None or gwa < highest_gwa:
+        if highest_gwa is None or gwa < highest_gwa:
             highest_gwa = gwa
 
 # the student who got the highest grade will be printed to the student who got the highest gwa
-best_student = name
+            best_student = name
+
+#print with color/highlight design
+
+print("\033[48;5;225m" + best_student + "\033[0m""\033[1;35mis the student with the highest GWA of\033[0m", "\033[48;5;225m" + str(highest_gwa), "\033[0m")
