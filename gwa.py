de8ba7b7a959fd9976eea756875a7e2864561082
Michael Angelo P. Biag <michaelangelobiag23@GMAIL.COM>
2023-04-29 02:10:16 +0800

    loop name and grade

--- gwa.py
+++ gwa.py
@@ -2,7 +2,15 @@
 #BSCPE 1-4
 # Write a Python program that read a file containing the name of 20 students together with their GWA. The program will outputs the name of the student who got the highest GWA (including the GWA).
 
+#before we start we need to add the list of students with grade
+with open("studentlist.txt", "r") as file:
+    for line in file:   
 
 #Create two variables with empty and no initial values to which we will append the values after analyzing each line of the text.
 highest_gwa = None
 best_student = ""
+
+#create two part, one is for student and the second one is for their grades
+name, gwa = line.strip().split(" - ")
+gwa = float(gwa)
+
