1d6c35436e8f3e7eed029837278c2bce230c224b
Michael Angelo P. Biag <michaelangelobiag23@GMAIL.COM>
2023-04-29 02:02:14 +0800

    this empty variables will include after the text

---
+++ gwa.py
@@ -0,0 +1,8 @@
+#Michael Angelo P. Biag
+#BSCPE 1-4
+# Write a Python program that read a file containing the name of 20 students together with their GWA. The program will outputs the name of the student who got the highest GWA (including the GWA).
+
+
+#Create two variables with empty and no initial values to which we will append the values after analyzing each line of the text.
+highest_gwa = None
+best_student = ""
